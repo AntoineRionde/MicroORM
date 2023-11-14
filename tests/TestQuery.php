c2914fa5996d325bc201f2761fe4ae1017fed9e6
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-11-14 16:23:06 +0100

    Maj du gitignore et ajout du contenu de TestQuery

--- tests/TestQuery.php
+++ tests/TestQuery.php
@@ -1,6 +1,15 @@
 <?php
+namespace iutnc\hellokant\tests;
+use iutnc\hellokant\query\Query;
+use PHPUnit\Framework\Attributes\DataProvider;
 
-class TestQuery
+class TestQuery extends \PHPUnit\Framework\TestCase
 {
+    public function testAccederCommande(){
+        $q = Query::table('commande');
+        $q->select(['id', 'numero', 'date', 'client_id']);
+        $res = $q->get();
+        echo $res;
+    }
 
 }
