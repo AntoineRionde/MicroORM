0e665fa61cae589ea2767867d798d8bb1dacd926
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-11-21 15:25:17 +0100

    Modif de TestQuery

--- tests/TestQuery.php
+++ tests/TestQuery.php
@@ -1,11 +1,17 @@
 <?php
 namespace iutnc\hellokant\tests;
+use iutnc\hellokant\factory\ConnectionFactory;
 use iutnc\hellokant\query\Query;
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
 
-class TestQuery extends \PHPUnit\Framework\TestCase
+class TestQuery extends TestCase
 {
     public function testAfficherSelect(){
+        // une seule fois au lancement de l'application
+        $conf = parse_ini_file('src/conf/db.conf.ini') ;
+        ConnectionFactory::makeConnection($conf);
+
         $q = Query::table('commande');
         $q->select(['id', 'numero', 'date', 'client_id']);
         $q->get();
