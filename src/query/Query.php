654da7aa6e3544e083f50c5ee9aef73e48d2af1c
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-11-21 15:04:36 +0100

    Modif de la classe Query

--- src/query/Query.php
+++ src/query/Query.php
@@ -1,6 +1,7 @@
 <?php
 namespace iutnc\hellokant\query;
 
+use iutnc\hellokant\factory\ConnectionFactory;
 use PDO;
 
 class Query
@@ -29,7 +30,7 @@ class Query
     public function get() : array|bool
     {
         $this->sql = "SELECT $this->fields FROM $this->sqltable";
-        if ($this->where) {
+        if ($this->where !== null) {
             $this->sql .= " WHERE $this->where";
         }
 
@@ -47,17 +48,48 @@ class Query
 
     public function delete(string $ligne) {
         $this->sql = "DELETE FROM $this->sqltable WHERE $ligne";
-        echo $this->sql;
+        $pdo = ConnectionFactory::getConnection();
+        $request = $pdo->prepare($this->sql);
+        $request->execute($this->args);
+        return $request->rowCount();
     }
 
     public function insert(array $fields) {
-        $this->sql = "INSERT INTO $this->sqltable VALUES (";
-        foreach ($fields as $key => $value) {
-            $this->sql .= "'$value',";
+//        $this->sql = "INSERT INTO $this->sqltable VALUES (";
+//        foreach ($fields as $key => $value) {
+//            $this->sql .= "'$value',";
+//        }
+//        $this->sql = substr($this->sql, 0, -1);
+//        $this->sql .= ")";
+//        echo $this->sql;
+
+        $tabsize = count($fields);
+        $compteur = 0;
+        $this->sql = "INSERT INTO " . $this->sqltable . " (";
+        foreach ($fields as $k => $v) {
+            $this->sql .= "$k";
+            $compteur++;
+            if (!($compteur == $tabsize)) {
+                $this->sql .= ',';
+            }
         }
-        $this->sql = substr($this->sql, 0, -1);
-        $this->sql .= ")";
-        echo $this->sql;
+        $this->sql .= ') VALUES (';
+        $compteur = 0;
+        foreach ($fields as $v) {
+            $this->args[] = $v;
+            $this->sql .= "?";
+            $compteur++;
+            if (!($compteur == $tabsize)) {
+                $this->sql .= ',';
+            }
+        }
+        $this->sql .= ')';
+
+        $pdo = ConnectionFactory::getConnection();
+        $request = $pdo->prepare($this->sql);
+        $request->execute($this->args);
+
+        return $pdo->lastInsertId($this->sqltable);
     }
 
 
