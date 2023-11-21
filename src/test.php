ba17a230db238977e0aa7fe471b751922bfadfa6
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-11-21 15:08:19 +0100

    Remove test php file

--- src/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-use iutnc\hellokant\query\Query;
-
-$q = Query::table('commande');
-$q->select(['id', 'numero', 'date', 'client_id']);
-$q->where('numero', '=', 1000);
-$q->get();
