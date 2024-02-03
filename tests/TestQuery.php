e9f9aba1ac4eea1ced4edc0a5c4262a64d7baea3
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-02-03 20:24:55 +0100

    Ajout de tests lié à aux méthodes : find, insert et delete, et belongs_to et has_many + maj du docker-compose.yml et du composer.json

--- tests/TestQuery.php
+++ tests/TestQuery.php
@@ -1,13 +1,12 @@
 <?php
 namespace iutnc\hellokant\tests;
 use iutnc\hellokant\factory\ConnectionFactory;
+use iutnc\hellokant\model\Article;
 use iutnc\hellokant\query\Query;
 use PHPUnit\Framework\TestCase;
 
 class TestQuery extends TestCase
 {
-
-    //setup connection before each test
 
     public function setUp(): void
     {
@@ -56,4 +55,66 @@ class TestQuery extends TestCase
         $res = $q->one();
         $this->assertEquals(65, $res['id']);
     }
+
+    public function testInsert()
+    {
+        $q = Query::table('article');
+        $art = new Article();
+        $art->nom = 'test';
+        $art->descr = 'test';
+        $art->tarif = 10;
+        $art->id_categ = 1;
+
+        $art->insert();
+        $res = $q->where('nom', '=', 'test')->one();
+        $this->assertEquals('test', $res['nom']);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testDelete()
+    {
+        $art = new Article();
+        $art->nom = 'test';
+        $art->descr = 'test';
+        $art->tarif = 10;
+        $art->id_categ = 1;
+        $art->insert();
+        $art->delete();
+
+        $q = Query::table('article');
+        $res = $q->where('nom', '=', 'test')->one();
+        $this->assertNull($res);
+
+    }
+
+
+    public function testFind()
+    {
+        $id = 65;
+        $article = Article::find($id);
+        $this->assertNotNull($article);
+        $this->assertEquals($id, $article['id']);
+    }
+
+    public function testBelongsTo()
+    {
+        $id = 65;
+        $categoryQuery = Query::table('categorie');
+        $category = $categoryQuery->where('id', '=', Article::find($id)['id_categ'])->one();
+        $this->assertNotNull($category);
+
+        $this->assertEquals('sport', $category['nom']);
+        $this->assertEquals('articles de sport', $category['descr']);
+    }
+
+    public function testHasMany()
+    {
+        $id = 1;
+        $articleQuery = Query::table('article');
+        $articles = $articleQuery->where('id_categ', '=', $id)->get();
+        $this->assertCount(3, $articles);
+    }
+
 }
